29722704d48c36e2a01e1e32b318121db4d8f4e1
schelip <fcominscheffel@gmail.com>
2021-10-19 21:08:52 -0300

    Atualizada Extensions.cs com os metodos Salvar e Carregar

--- Helpers/Extensions.cs
+++ Helpers/Extensions.cs
@@ -1,4 +1,8 @@
 using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Xml;
 
 namespace Dio.CadastroMidia.Helpers
 {
@@ -20,5 +24,61 @@ namespace Dio.CadastroMidia.Helpers
         {
             return obj.GetType().GetMethod(name).Invoke(obj, parameters);
         }
+
+		public static bool isDiretorioVazio(string path)
+		{
+			return !Directory.EnumerateFileSystemEntries(path).Any();
+		}
+
+        public static void Salvar(string path, object obj)
+        {
+			string filename = path.Split('\\').Last();
+			Console.WriteLine($"Salvando {filename} ...");
+			try
+			{
+				using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+				using (XmlWriter writer = XmlWriter.Create(fs, new XmlWriterSettings{Indent = true}))
+				{
+					DataContractSerializer serializer = new DataContractSerializer(obj.GetType());
+					serializer.WriteObject(writer, obj);
+				}
+
+				Console.WriteLine("Dados salvos com sucesso!");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+        }
+
+		public static object Carregar(string path, object obj)
+		{
+			string filename = path.Split('\\').Last();
+			Console.WriteLine($"Carregando {filename} ...");
+			
+			try
+			{
+				using (FileStream fs = new FileStream(path, FileMode.Open))
+				using (XmlDictionaryReader reader = XmlDictionaryReader.CreateTextReader(fs, new XmlDictionaryReaderQuotas()))
+				{
+
+					DataContractSerializer serializer = new DataContractSerializer(obj.GetType());
+
+					obj = serializer.ReadObject(reader, true);
+				}
+
+				Console.WriteLine($"{filename} com sucesso!");
+			}
+			catch (FileNotFoundException)
+			{
+				Console.WriteLine("Nenhum arquivo de dados encontrado");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+
+			return obj;
+		}
     }
 }
